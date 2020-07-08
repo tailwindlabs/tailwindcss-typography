1132cbd9895d76615411e503e89cef6a13209904
Brad Cornes <bradlc41@gmail.com>
2020-07-08 13:03:48 +0100

    Rework styles.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const plugin = require('tailwindcss/plugin')
 const merge = require('lodash/merge')
+const castArray = require('lodash/castArray')
 const styles = require('./styles')
 // const union = require('lodash/union')
 
@@ -14,12 +15,12 @@ function configToCss(config) {
       .reduce((acc, cur) => {
         return { ...acc, ...computed[cur](config[cur]) }
       }, {}),
-    ...(config.css || {}),
+    ...merge(...castArray(config.css || {})),
   }
 }
 
 module.exports = plugin.withOptions(
-  ({ modifiers = ['default', 'sm', 'lg', 'xl', '2xl'] } = {}) => {
+  ({ modifiers = ['sm', 'lg', 'xl', '2xl'] } = {}) => {
     return function ({ addComponents, theme, variants }) {
       const config = theme('typography', {})
 
@@ -42,17 +43,14 @@ module.exports = plugin.withOptions(
         [`@variants ${variants('typography').join(', ')}`]: [
           {
             '.prose': merge(
-              styles.shared,
-              !modifiers.includes('default')
-                ? {}
-                : styles.modifiers.default.css,
+              ...castArray(styles.default.css),
               configToCss(config.default || {})
             ),
             ...(!modifiers.includes('sm')
               ? {}
               : {
                   '.prose-sm': merge(
-                    styles.modifiers.sm.css,
+                    ...castArray(styles.sm.css),
                     configToCss(config.sm || {})
                   ),
                 }),
@@ -60,7 +58,7 @@ module.exports = plugin.withOptions(
               ? {}
               : {
                   '.prose-lg': merge(
-                    styles.modifiers.lg.css,
+                    ...castArray(styles.lg.css),
                     configToCss(config.lg || {})
                   ),
                 }),
@@ -68,7 +66,7 @@ module.exports = plugin.withOptions(
               ? {}
               : {
                   '.prose-xl': merge(
-                    styles.modifiers.xl.css,
+                    ...castArray(styles.xl.css),
                     configToCss(config.xl || {})
                   ),
                 }),
@@ -76,7 +74,7 @@ module.exports = plugin.withOptions(
               ? {}
               : {
                   '.prose-2xl': merge(
-                    styles.modifiers['2xl'].css,
+                    ...castArray(styles['2xl'].css),
                     configToCss(config['2xl'] || {})
                   ),
                 }),
