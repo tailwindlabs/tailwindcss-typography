40b66fb5c8a69251b64c92493ff61f19f202ab1b
Adam Wathan <adam.wathan@gmail.com>
2020-07-09 09:48:12 -0400

    Add temporary additional sample content to test against

--- docs/pages/index.js
+++ docs/pages/index.js
@@ -1,9 +1,22 @@
 import { useState } from 'react'
 import Layout from '../components/Layout'
 import MarkdownSample from '../components/MarkdownSample.mdx'
+import MarkdownSampleB from '../components/MarkdownSampleB.mdx'
+import MarkdownSampleC from '../components/MarkdownSampleC.mdx'
+import MarkdownSampleD from '../components/MarkdownSampleD.mdx'
+import MarkdownSampleE from '../components/MarkdownSampleE.mdx'
 
 export default () => {
   const [size, setSize] = useState('default')
+  const [sample, setSample] = useState('A')
+
+  const SampleComponent = {
+    A: MarkdownSample,
+    B: MarkdownSampleB,
+    C: MarkdownSampleC,
+    D: MarkdownSampleD,
+    E: MarkdownSampleE,
+  }[sample]
 
   return (
     <Layout meta={{ title: 'Tailwind CSS Typography' }}>
@@ -12,17 +25,25 @@ export default () => {
           <button
             key={s}
             type="button"
-            className={`border mt-3 px-4 focus:outline-none ${
-              size === s ? 'border-black' : ''
-            }`}
+            className={`border mt-3 px-4 focus:outline-none ${size === s ? 'border-black' : ''}`}
             onClick={() => setSize(s)}
           >
             {s}
           </button>
         ))}
+        {['A', 'B', 'C', 'D', 'E'].map((s) => (
+          <button
+            key={s}
+            type="button"
+            className={`border mt-3 px-4 focus:outline-none ${sample === s ? 'border-black' : ''}`}
+            onClick={() => setSample(s)}
+          >
+            Sample {s}
+          </button>
+        ))}
       </div>
       <div className={`prose prose-${size} mx-auto`}>
-        <MarkdownSample />
+        <SampleComponent />
       </div>
     </Layout>
   )
