84e508244048a5d2be7a3bc15c96158a6dba1465
Brad Cornes <bradlc41@gmail.com>
2020-07-13 10:30:06 +0100

    remove runtime js

--- docs/pages/index.js
+++ docs/pages/index.js
@@ -3,25 +3,15 @@ import Layout from '../components/Layout'
 import MarkdownSample from '../components/MarkdownSample.mdx'
 
 export default () => {
-  const [size, setSize] = useState('default')
-
   return (
     <Layout meta={{ title: 'Tailwind CSS Typography' }}>
-      <div className="fixed top-0 left-0 flex flex-col ml-3">
-        {['sm', 'default', 'lg', 'xl', '2xl'].map((s) => (
-          <button
-            key={s}
-            type="button"
-            className={`border mt-3 px-4 focus:outline-none ${size === s ? 'border-black' : ''}`}
-            onClick={() => setSize(s)}
-          >
-            {s}
-          </button>
-        ))}
-      </div>
-      <div className={`prose prose-${size} mx-auto`}>
+      <div className="prose mx-auto">
         <MarkdownSample />
       </div>
     </Layout>
   )
+}
+
+export const config = {
+  unstable_runtimeJS: false
 }
