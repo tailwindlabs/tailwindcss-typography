ca80f83bf366924143a1a4c346a23a60d0448bd4
Peter Neupauer <peter@neupauer.sk>
2020-09-14 19:31:01 +0200

    Preserve `type` attribute of the `ol` element

--- src/styles.js
+++ src/styles.js
@@ -28,12 +28,39 @@ module.exports = {
         ol: {
           counterReset: 'list-counter',
         },
+        'ol[type=A]': {
+          '--list-counter-style': 'upper-alpha',
+        },
+        'ol[type=a]': {
+          '--list-counter-style': 'lower-alpha',
+        },
+        'ol[type=A s]': {
+          '--list-counter-style': 'upper-alpha',
+        },
+        'ol[type=a s]': {
+          '--list-counter-style': 'lower-alpha',
+        },
+        'ol[type=I]': {
+          '--list-counter-style': 'upper-roman',
+        },
+        'ol[type=i]': {
+          '--list-counter-style': 'lower-roman',
+        },
+        'ol[type=I s]': {
+          '--list-counter-style': 'upper-roman',
+        },
+        'ol[type=i s]': {
+          '--list-counter-style': 'lower-roman',
+        },
+        'ol[type=1]': {
+          '--list-counter-style': 'decimal',
+        },
         'ol > li': {
           position: 'relative',
           counterIncrement: 'list-counter',
         },
         'ol > li::before': {
-          content: 'counter(list-counter) "."',
+          content: 'counter(list-counter, var(--list-counter-style, decimal)) "."',
           position: 'absolute',
           fontWeight: '400',
           color: defaultTheme.colors.gray[600],
