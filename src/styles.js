5740c59c0686d18efafd6c3efada803566072d73
Kristóf Poduszló <kripod@protonmail.com>
2020-12-31 15:38:07 +0100

    refactor: wrap light modifier code inside an array

--- src/styles.js
+++ src/styles.js
@@ -328,63 +328,65 @@ module.exports = (theme) => ({
     ],
   },
   light: {
-    css: {
-      color: theme('colors.gray.300', defaultTheme.colors.gray[300]),
-      '[class~="lead"]': {
+    css: [
+      {
         color: theme('colors.gray.300', defaultTheme.colors.gray[300]),
+        '[class~="lead"]': {
+          color: theme('colors.gray.300', defaultTheme.colors.gray[300]),
+        },
+        a: {
+          color: theme('colors.gray.200', defaultTheme.colors.gray[200]),
+        },
+        strong: {
+          color: theme('colors.gray.200', defaultTheme.colors.gray[200]),
+        },
+        'ol > li::before': {
+          color: theme('colors.gray.400', defaultTheme.colors.gray[400]),
+        },
+        'ul > li::before': {
+          backgroundColor: theme('colors.gray.600', defaultTheme.colors.gray[600]),
+        },
+        hr: {
+          borderColor: theme('colors.gray.700', defaultTheme.colors.gray[700]),
+        },
+        blockquote: {
+          color: theme('colors.gray.200', defaultTheme.colors.gray[200]),
+          borderLeftColor: theme('colors.gray.700', defaultTheme.colors.gray[700]),
+        },
+        h1: {
+          color: theme('colors.gray.200', defaultTheme.colors.gray[200]),
+        },
+        h2: {
+          color: theme('colors.gray.200', defaultTheme.colors.gray[200]),
+        },
+        h3: {
+          color: theme('colors.gray.200', defaultTheme.colors.gray[200]),
+        },
+        h4: {
+          color: theme('colors.gray.200', defaultTheme.colors.gray[200]),
+        },
+        'figure figcaption': {
+          color: theme('colors.gray.400', defaultTheme.colors.gray[400]),
+        },
+        code: {
+          color: theme('colors.gray.200', defaultTheme.colors.gray[200]),
+        },
+        'a code': {
+          color: theme('colors.gray.200', defaultTheme.colors.gray[200]),
+        },
+        pre: {
+          color: theme('colors.gray.700', defaultTheme.colors.gray[700]),
+          backgroundColor: theme('colors.gray.800', defaultTheme.colors.gray[800]),
+        },
+        thead: {
+          color: theme('colors.gray.200', defaultTheme.colors.gray[200]),
+          borderBottomColor: theme('colors.gray.600', defaultTheme.colors.gray[600]),
+        },
+        'tbody tr': {
+          borderBottomColor: theme('colors.gray.700', defaultTheme.colors.gray[700]),
+        },
       },
-      a: {
-        color: theme('colors.gray.200', defaultTheme.colors.gray[200]),
-      },
-      strong: {
-        color: theme('colors.gray.200', defaultTheme.colors.gray[200]),
-      },
-      'ol > li::before': {
-        color: theme('colors.gray.400', defaultTheme.colors.gray[400]),
-      },
-      'ul > li::before': {
-        backgroundColor: theme('colors.gray.600', defaultTheme.colors.gray[600]),
-      },
-      hr: {
-        borderColor: theme('colors.gray.700', defaultTheme.colors.gray[700]),
-      },
-      blockquote: {
-        color: theme('colors.gray.200', defaultTheme.colors.gray[200]),
-        borderLeftColor: theme('colors.gray.700', defaultTheme.colors.gray[700]),
-      },
-      h1: {
-        color: theme('colors.gray.200', defaultTheme.colors.gray[200]),
-      },
-      h2: {
-        color: theme('colors.gray.200', defaultTheme.colors.gray[200]),
-      },
-      h3: {
-        color: theme('colors.gray.200', defaultTheme.colors.gray[200]),
-      },
-      h4: {
-        color: theme('colors.gray.200', defaultTheme.colors.gray[200]),
-      },
-      'figure figcaption': {
-        color: theme('colors.gray.400', defaultTheme.colors.gray[400]),
-      },
-      code: {
-        color: theme('colors.gray.200', defaultTheme.colors.gray[200]),
-      },
-      'a code': {
-        color: theme('colors.gray.200', defaultTheme.colors.gray[200]),
-      },
-      pre: {
-        color: theme('colors.gray.700', defaultTheme.colors.gray[700]),
-        backgroundColor: theme('colors.gray.800', defaultTheme.colors.gray[800]),
-      },
-      thead: {
-        color: theme('colors.gray.200', defaultTheme.colors.gray[200]),
-        borderBottomColor: theme('colors.gray.600', defaultTheme.colors.gray[600]),
-      },
-      'tbody tr': {
-        borderBottomColor: theme('colors.gray.700', defaultTheme.colors.gray[700]),
-      },
-    },
+    ],
   },
   sm: {
     css: [
