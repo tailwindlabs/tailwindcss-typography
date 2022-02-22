66baf9bdf75663732168cced74beb3f98403c864
Robin Malfait <malfait.robin@gmail.com>
2022-02-22 11:28:23 +0100

    Improve tests for Tailwind CSS v3 (#252)
    * Adjust tailwind.config.js to match v3 (drop purge: entry)
    
    * Adjust tests to tw3 non-alpha output
    
    * run prettier on test file
    
    * rename `twVariables` to `defaults`
    
    This is more inline with what we do in the tailwindcss codebase itself
    
    * embed the `${defaults}` directly
    
    Co-authored-by: Ingo Struck (git commits) <gitlab@ingostruck.de>

--- demo/tailwind.config.js
+++ demo/tailwind.config.js
@@ -1,11 +1,9 @@
 const mdx = require('@mdx-js/mdx')
 
 module.exports = {
-  purge: {
-    content: ['./demo/pages/**/*.{js,mdx}', './demo/components/**/*.{js,mdx}'],
-    transform: {
-      mdx: (content) => mdx.sync(content),
-    },
+  content: ['./demo/pages/**/*.{js,mdx}', './demo/components/**/*.{js,mdx}'],
+  transform: {
+    mdx: (content) => mdx.sync(content),
   },
   theme: {
     extend: {
