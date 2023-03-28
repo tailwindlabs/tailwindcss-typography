20382a41d0680295f8a02109f1bd265e6722e971
Lev Myndra <talaxasy.or.lev@gmail.com>
2023-03-28 14:30:25 +0300

    Allow to import such utils as round, rem, em
    When a person wants to extend the typography config not only colors, e.g.:
    
    ```
    extend: {
          typography: ({ theme }) => ({
            lg: {
              css: {
                h1: {
                  fontSize: em(38, 18),
                  marginTop: '0',
                  marginBottom: em(40, 48),
                  lineHeight: round(48 / 48),
                },
              },
            },
    ```

--- src/styles.js
+++ src/styles.js
@@ -1,12 +1,12 @@
 const colors = require('tailwindcss/colors')
 
-const round = (num) =>
+export const round = (num) =>
   num
     .toFixed(7)
     .replace(/(\.[0-9]+?)0+$/, '$1')
     .replace(/\.0$/, '')
-const rem = (px) => `${round(px / 16)}rem`
-const em = (px, base) => `${round(px / base)}em`
+export const rem = (px) => `${round(px / 16)}rem`
+export const em = (px, base) => `${round(px / base)}em`
 
 let defaultModifiers = {
   sm: {
