881b0488df9fd05e5361276b66a9ee8e7f39a3a7
Robin Malfait <malfait.robin@gmail.com>
2026-05-12 15:11:49 +0200

    Setup OIDC (#423)
    This PR sets up OIDC publishing by merging the workflows first

--- scripts/release-channel.js
+++ scripts/release-channel.js
@@ -10,7 +10,7 @@
 let version =
   process.argv[2] || process.env.npm_package_version || require('../package.json').version
 
-let match = /\d+\.\d+\.\d+-(.*)\.\d+/g.exec(version)
+let match = /\d+\.\d+\.\d+-(.*)(\.\d+)?/g.exec(version)
 if (match) {
   console.log(match[1])
 } else {
